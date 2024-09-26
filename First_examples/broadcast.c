9d7ef36566784484c036e78cbd12116ad75fed28
Dorijan Di Zepp <dorijan.dizepp@gmail.com>
2024-09-26 12:52:57 +0200

    ring & broadcast exercises

---
+++ First_examples/broadcast.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+const int MAX_STRING = 100;
+
+/**
+ a process 'p' wants to share the content of a variable 'a'
+ with all the other processes
+ */
+
+int main(int argc, char** argv){
+    char greeting[MAX_STRING];
+    int comm_sz; //number of processes
+    int my_rank; //my process rank
+    int p = 0; //sender rank
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    //get the sender rank (if valid)
+    if (argc >=2){
+        p = atoi(argv[1]);
+        if(p < 0 || p >= comm_sz){
+            if(my_rank == 0)
+                printf("Non valid sender rank. Needs to be in range 0 - %d\n", comm_sz-1);
+            MPI_Finalize();
+            return 0;
+        }
+    } else {
+        if(my_rank == 0)
+            printf("Please, indicate which process is the sender\nmpiexec -n <x> <exec> <sender_rank>\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    if(my_rank != p){
+        //i am the receiver of the broadcast message
+        MPI_Recv(greeting, MAX_STRING, MPI_CHAR, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d received: %s\n", my_rank, greeting);
+    }else{
+        //i am the sender
+        printf("Sender started (Rank: %d)\n", my_rank);
+        for(int q=0; q<comm_sz; q++){
+            if(q != p){
+                //do not send to myself
+                sprintf(greeting, "Sending from sender %d to everyone!", p);
+                MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, q, 0, MPI_COMM_WORLD);
+
+            }
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ First_examples/ring.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+const int MAX_STRING = 100;
+
+/**
+ a program that takes data from process zero and sends it to all of the pìother processes
+ by sending it in a ring. That is, process i should receive the data and send it oto process i+1,
+ until the last process is reached.
+ */
+
+int main(int argc, char** argv){
+    char greeting[MAX_STRING];
+    int comm_sz; //number of processes
+    int my_rank; //my process rank
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    if(my_rank != 0){
+        //simply wait the i-1 process messagge and then forward it to the i+1.
+        //if last one, simply print and end
+        MPI_Recv(greeting, MAX_STRING, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d received: %s\n", my_rank, greeting);
+
+        if(my_rank < (comm_sz - 1)){
+            sprintf(greeting, "Sending from process %d to %d!", my_rank, my_rank+1);
+            MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+        }
+    }else{
+        //'main' process
+        //Send it to the first child!
+        printf("Process 0 started\n");
+        if(comm_sz > 1){
+            sprintf(greeting, "Sending from process %d to %d!", my_rank, my_rank+1);
+            MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+        } else {
+            printf("No child process to forward\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
